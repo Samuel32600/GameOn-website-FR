76fbddc5aa26c0ba64fa462b11c488a96f9a909f
Samuel32600 <samuel.parmentier@orange.fr>
2021-05-11 15:30:34 +0200

    ajout de commentaire + fonctionnalité du bonton X

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -4,13 +4,18 @@ function editNav() {
     x.className += " responsive";
   } else {
     x.className = "topnav";
-  }
+  }  
 }
 
 // DOM Elements
+// constance to open the form
 const modalbg = document.querySelector(".bground");
+
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+
+//constance to close the form
+const iconCloseForm = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +24,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
+// event click to close windows
+iconCloseForm.addEventListener("click", closePopUp)
+
+//define function
+function closePopUp() {
+modalbg.style.display ="none"}
+
+
 
 
