8c02b21d5b2c320b08ff0bdb6cd84867480b4edf
Samuel32600 <samuel.parmentier@orange.fr>
2021-05-17 16:28:47 +0200

    update modale 1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -4,34 +4,47 @@ function editNav() {
     x.className += " responsive";
   } else {
     x.className = "topnav";
-  }  
+  }
 }
 
 // DOM Elements
-// constance to open the form
+
+//details to open the form
+// constante for the form
 const modalbg = document.querySelector(".bground");
-
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-//constance to close the form
-const iconCloseForm = document.querySelector(".close");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
 
-// event click to close windows
-iconCloseForm.addEventListener("click", closePopUp)
+//constante for button register
+const modalBtn = document.querySelectorAll(".modal-btn");
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-//define function
+
+//Detail to close form
+//constante to close the form
+const iconCloseForm = document.querySelector("span.close").addEventListener("click", closePopUp)
+//close 
 function closePopUp() {
-modalbg.style.display ="none"}
+  modalbg.style.display = "none";
+}
+
+const formData = document.querySelectorAll(".formData");
+
+const firstName = document.getElementById("first");
+const familyName = document.getElementById("last");
+const mail = document.getElementById("email");
+const date = document.getElementById("birthdate");
+const competitionNumber = document.getElementById("quantity");
+const location1 = document.getElementById("location1");
+const location2 = document.getElementById("location2");
+const location3 = document.getElementById("location3");
+const location4 = document.getElementById("location4");
+const location5 = document.getElementById("location5");
+const location6 = document.getElementById("location6");
 
 
 
