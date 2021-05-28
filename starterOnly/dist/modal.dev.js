8c777db12a659864e37203f3c6ae370bff50e8b5
Samuel32600 <samuel.parmentier@orange.fr>
2021-05-28 16:54:39 +0200

    test pop up

--- starterOnly/dist/modal.dev.js
+++ starterOnly/dist/modal.dev.js
@@ -37,82 +37,87 @@ function closePopUp() {
 document.getElementById("myForm").addEventListener("submit", validation);
 
 function validation(e) {
-  var form_OK = true; //-----first Name
+  var form_OK = true; //-----first Name-----
 
   var firstName = document.getElementById("first").value;
-  var firstError = document.getElementById("first-error");
+  var firstError = document.getElementById("first-error"); // First Valid
 
   if (firstName !== null && firstName.length > 1) {
     console.log('le prenom est correct');
     firstError.innerText = "";
     firstError.parentNode.removeAttribute("data-error-visible");
-  } else {
-    console.log('le prenom est incorrect');
-    firstError.parentNode.setAttribute("data-error", errorMessages.first);
-    firstError.parentNode.setAttribute("data-error-visible", "true");
-    form_OK = false;
-  } //-----name
+  } // First InValid
+  else {
+      console.log('le prenom est incorrect');
+      firstError.parentNode.setAttribute("data-error", errorMessages.first);
+      firstError.parentNode.setAttribute("data-error-visible", "true");
+      form_OK = false;
+    } //-----name-----
 
 
   var name = document.getElementById("last").value;
-  var nameError = document.getElementById("last-error");
+  var nameError = document.getElementById("last-error"); // Name Valid
 
   if (name !== null && name.length > 1) {
     console.log('le nom est correct');
     nameError.innerText = "";
     nameError.parentNode.removeAttribute("data-error-visible");
-  } else {
-    console.log('le nom est incorrect');
-    nameError.parentNode.setAttribute("data-error", errorMessages.last);
-    nameError.parentNode.setAttribute("data-error-visible", "true");
-    form_OK = false;
-  } //-----email
+  } // Name InValid
+  else {
+      console.log('le nom est incorrect');
+      nameError.parentNode.setAttribute("data-error", errorMessages.last);
+      nameError.parentNode.setAttribute("data-error-visible", "true");
+      form_OK = false;
+    } //-----email-----
 
 
   var email = document.getElementById("email").value;
   var emailAttribut = /^[a-z\d_\-]+(\.[\a-z\d\-]+)*@[a-z\d\-]+(\.[a-z\d]+)+$/;
-  var emailError = document.getElementById("email-error");
+  var emailError = document.getElementById("email-error"); // Email Valid
 
   if (emailAttribut.exec(email)) {
     console.log('le mail est valide');
     emailError.innerText = "";
     emailError.parentNode.removeAttribute("data-error-visible");
-  } else {
-    console.log('le mail est invalide');
-    emailError.parentNode.setAttribute("data-error", errorMessages.email);
-    emailError.parentNode.setAttribute("data-error-visible", "true");
-    form_OK = false;
-  } //-----birthdate
+  } // Email InValid
+  else {
+      console.log('le mail est invalide');
+      emailError.parentNode.setAttribute("data-error", errorMessages.email);
+      emailError.parentNode.setAttribute("data-error-visible", "true");
+      form_OK = false;
+    } //-----birthdate-----
 
 
   var birthdateAttribut = document.getElementById("birthdate").value;
-  var birthdateError = document.getElementById("birthdate-error");
+  var birthdateError = document.getElementById("birthdate-error"); // Birhdate Valid
 
   if (birthdateAttribut !== "") {
     console.log('la date est valide');
     birthdateError.innerText = "";
     birthdateError.parentNode.removeAttribute("data-error-visible");
-  } else {
-    console.log('le date est invalide');
-    birthdateError.parentNode.setAttribute("data-error", errorMessages.birthdate);
-    birthdateError.parentNode.setAttribute("data-error-visible", "true");
-    form_OK = false;
-  } //-----quantity
+  } // Birhdate InValid
+  else {
+      console.log('le date est invalide');
+      birthdateError.parentNode.setAttribute("data-error", errorMessages.birthdate);
+      birthdateError.parentNode.setAttribute("data-error-visible", "true");
+      form_OK = false;
+    } //-----quantity-----
 
 
   var quantity = document.getElementById("quantity").value;
-  var quantityError = document.getElementById("quantity-error");
+  var quantityError = document.getElementById("quantity-error"); // Quantity Valid
 
   if (quantity > -1 && quantity < 100 && quantity != "") {
     console.log('la quantité est correcte');
     quantityError.innerText = "";
     quantityError.parentNode.removeAttribute("data-error-visible");
-  } else {
-    console.log('la quantité est incorrecte');
-    quantityError.parentNode.setAttribute("data-error", errorMessages.quantity);
-    quantityError.parentNode.setAttribute("data-error-visible", "true");
-    form_OK = false;
-  } //-----city
+  } // Quantity InValid
+  else {
+      console.log('la quantité est incorrecte');
+      quantityError.parentNode.setAttribute("data-error", errorMessages.quantity);
+      quantityError.parentNode.setAttribute("data-error-visible", "true");
+      form_OK = false;
+    } //-----Location-----
 
 
   var NewYork = document.getElementById("location1");
@@ -121,39 +126,45 @@ function validation(e) {
   var Chicago = document.getElementById("location4");
   var Boston = document.getElementById("location5");
   var Portland = document.getElementById("location6");
-  var cityError = document.getElementById("location-error");
+  var cityError = document.getElementById("location-error"); // Location Valid
 
   if (NewYork.checked || SanFrancisco.checked || Seattle.checked || Chicago.checked || Boston.checked || Portland.checked) {
     console.log('une ville est bien sectionnée');
     cityError.innerText = "";
     cityError.parentNode.removeAttribute("data-error-visible");
-  } else {
-    console.log('aucune ville de selectionner');
-    cityError.parentNode.setAttribute("data-error", errorMessages.location);
-    cityError.parentNode.setAttribute("data-error-visible", "true");
-    form_OK = false;
-  } //-----Condition
+  } // Location InValid
+  else {
+      console.log('aucune ville de selectionner');
+      cityError.parentNode.setAttribute("data-error", errorMessages.location);
+      cityError.parentNode.setAttribute("data-error-visible", "true");
+      form_OK = false;
+    } //-----Condition-----
 
 
   var conditionCheckbox = document.getElementById("checkbox1");
-  var conditionError = document.getElementById("condition-error");
+  var conditionError = document.getElementById("condition-error"); // Condition Valid
 
   if (conditionCheckbox.checked) {
     console.log('les conditions sont bien cochées');
     conditionError.innerText = "";
     conditionError.parentNode.removeAttribute("data-error-visible");
-  } else {
-    console.log('les conditions sont  non cochées');
-    conditionError.parentNode.setAttribute("data-error", errorMessages.condition);
-    conditionError.parentNode.setAttribute("data-error-visible", "true");
-    form_OK = false;
+  } // Condition InValid
+  else {
+      console.log('les conditions sont  non cochées');
+      conditionError.parentNode.setAttribute("data-error", errorMessages.condition);
+      conditionError.parentNode.setAttribute("data-error-visible", "true");
+      form_OK = false;
+    } //-----form valide
+
+
+  if (form_OK) {
+    console.log('le formulaire est valide');
+    validation = true;
   } //-----form non valide
-
-
-  if (!form_OK) {
-    e.preventDefault();
-    console.log('le formulaire est non valide');
-  }
+  else {
+      e.preventDefault();
+      console.log('le formulaire est non valide');
+    }
 } //error messages
 
 
@@ -165,4 +176,14 @@ var errorMessages = {
   quantity: "Veuillez entrer un nombre valide compris entre 0 et 99",
   location: "Veuillez choisir une ville.",
   condition: "Veuillez accepter les conditions d'utilisations."
-};
+}; //details to open the form
+// constante for the form
+
+var popUpFinal = document.querySelector(".form-endvalidation");
+document.querySelector('.btn-submit').addEventListener("click", PopUpValidation); // launch modal form
+
+function PopUpValidation() {
+  if (validation === true) {
+    popUpFinal.style.display = "block";
+  }
+}
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -39,32 +39,33 @@ function validation(e) {
 
   let form_OK = true;
 
-  //-----first Name
+  //-----first Name-----
   let firstName = document.getElementById("first").value;
   const firstError = document.getElementById("first-error");
-
+  // First Valid
   if (firstName !== null && firstName.length > 1) {
     console.log('le prenom est correct');
-    firstError.innerText = ""; 
+    firstError.innerText = "";
     firstError.parentNode.removeAttribute("data-error-visible");
   }
+  // First InValid
   else {
-    console.log('le prenom est incorrect');    
+    console.log('le prenom est incorrect');
     firstError.parentNode.setAttribute("data-error", errorMessages.first);
     firstError.parentNode.setAttribute("data-error-visible", "true");
     form_OK = false;
   }
 
-  //-----name
+  //-----name-----
   let name = document.getElementById("last").value;
   const nameError = document.getElementById("last-error");
-
+  // Name Valid
   if (name !== null && name.length > 1) {
     console.log('le nom est correct');
     nameError.innerText = "";
     nameError.parentNode.removeAttribute("data-error-visible");
-
   }
+  // Name InValid
   else {
     console.log('le nom est incorrect');
     nameError.parentNode.setAttribute("data-error", errorMessages.last);
@@ -72,16 +73,17 @@ function validation(e) {
     form_OK = false;
   }
 
-  //-----email
+  //-----email-----
   let email = document.getElementById("email").value;
   let emailAttribut = /^[a-z\d_\-]+(\.[\a-z\d\-]+)*@[a-z\d\-]+(\.[a-z\d]+)+$/;
   const emailError = document.getElementById("email-error");
-
+  // Email Valid
   if (emailAttribut.exec(email)) {
     console.log('le mail est valide');
     emailError.innerText = "";
-    emailError.parentNode.removeAttribute("data-error-visible"); 
+    emailError.parentNode.removeAttribute("data-error-visible");
   }
+  // Email InValid
   else {
     console.log('le mail est invalide');
     emailError.parentNode.setAttribute("data-error", errorMessages.email);
@@ -89,15 +91,16 @@ function validation(e) {
     form_OK = false;
   }
 
-  //-----birthdate
+  //-----birthdate-----
   let birthdateAttribut = document.getElementById("birthdate").value;
   const birthdateError = document.getElementById("birthdate-error");
-
+  // Birhdate Valid
   if (birthdateAttribut !== "") {
     console.log('la date est valide');
     birthdateError.innerText = "";
     birthdateError.parentNode.removeAttribute("data-error-visible");
   }
+  // Birhdate InValid
   else {
     console.log('le date est invalide');
     birthdateError.parentNode.setAttribute("data-error", errorMessages.birthdate);
@@ -105,15 +108,16 @@ function validation(e) {
     form_OK = false;
   }
 
-  //-----quantity
+  //-----quantity-----
   let quantity = document.getElementById("quantity").value;
   const quantityError = document.getElementById("quantity-error");
-
+  // Quantity Valid
   if (quantity > -1 && quantity < 100 && quantity != "") {
     console.log('la quantité est correcte');
     quantityError.innerText = "";
     quantityError.parentNode.removeAttribute("data-error-visible");
   }
+  // Quantity InValid
   else {
     console.log('la quantité est incorrecte');
     quantityError.parentNode.setAttribute("data-error", errorMessages.quantity);
@@ -121,7 +125,7 @@ function validation(e) {
     form_OK = false;
   }
 
-  //-----city
+  //-----Location-----
   let NewYork = document.getElementById("location1");
   let SanFrancisco = document.getElementById("location2");
   let Seattle = document.getElementById("location3");
@@ -129,12 +133,13 @@ function validation(e) {
   let Boston = document.getElementById("location5");
   let Portland = document.getElementById("location6");
   const cityError = document.getElementById("location-error");
-
+  // Location Valid
   if (NewYork.checked || SanFrancisco.checked || Seattle.checked || Chicago.checked || Boston.checked || Portland.checked) {
     console.log('une ville est bien sectionnée');
     cityError.innerText = "";
     cityError.parentNode.removeAttribute("data-error-visible");
   }
+  // Location InValid
   else {
     console.log('aucune ville de selectionner');
     cityError.parentNode.setAttribute("data-error", errorMessages.location);
@@ -142,23 +147,31 @@ function validation(e) {
     form_OK = false;
   }
 
-  //-----Condition
+  //-----Condition-----
   let conditionCheckbox = document.getElementById("checkbox1");
   const conditionError = document.getElementById("condition-error")
+  // Condition Valid
   if (conditionCheckbox.checked) {
     console.log('les conditions sont bien cochées');
     conditionError.innerText = "";
-    conditionError.parentNode.removeAttribute("data-error-visible");    
+    conditionError.parentNode.removeAttribute("data-error-visible");
   }
+  // Condition InValid
   else {
-    console.log('les conditions sont  non cochées');    
+    console.log('les conditions sont  non cochées');
     conditionError.parentNode.setAttribute("data-error", errorMessages.condition);
     conditionError.parentNode.setAttribute("data-error-visible", "true");
     form_OK = false;
   }
 
+  //-----form valide
+  if (form_OK) {
+    console.log('le formulaire est valide');
+    validation = true;
+  }
+
   //-----form non valide
-  if (!form_OK) {
+  else {
     e.preventDefault();
     console.log('le formulaire est non valide');
   }
@@ -174,4 +187,16 @@ let errorMessages = {
   location: "Veuillez choisir une ville.",
   condition: "Veuillez accepter les conditions d'utilisations.",
 }
+
+//details to open the form
+// constante for the form
+const popUpFinal = document.querySelector(".form-endvalidation");
+document.querySelector('.btn-submit').addEventListener("click", PopUpValidation);
+// launch modal form
+function PopUpValidation() {
+  if (validation === true) {
+    popUpFinal.style.display = "block";
+  }
+}
+
 
